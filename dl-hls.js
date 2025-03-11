81b0f5d26b0719343a9c29ed36296e6aaca4bb12
Muubk <19837509+unknowns123@users.noreply.github.com>
2025-03-11 21:32:39 +0900

    First

---
+++ dl-hls.js
@@ -0,0 +1,33 @@
+//Download m3u file from some VOD services
+
+
+//For http live streaming service (This script is generally compatible with smartphones and similar devices.)
+const video = document.querySelector('video'); 
+video && video.src ? console.log("Extracted m3u8 link:", video.src) : console.log("m3u8 link not found.");
+
+
+//For some video services (From computer)
+(async () => {
+  const m3uUrls = [];
+  const entries = performance.getEntries();
+
+  for (const entry of entries) {
+    if (entry.name.includes(".m3u")) {
+      m3uUrls.push(entry.name);
+    }
+  }
+
+  for(const url of m3uUrls){
+    try {
+      const response = await fetch(url, { method: 'HEAD' });
+      if (response.ok) {
+        console.log("M3U URL:", url);
+      }
+    }catch(error) {
+      console.error("error fetching URL:", url, error);
+    }
+  }
+
+  console.log("Found M3U URLs:", m3uUrls);
+
+})();
