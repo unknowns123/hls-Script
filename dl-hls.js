78f86c079efd864def1830c846a88a3a5d7e81a1
Mofax <19837509+unknowns123@users.noreply.github.com>
2025-03-15 15:56:23 +0900

    added mp4

--- dl-hls.js
+++ dl-hls.js
@@ -1,4 +1,4 @@
-//Download m3u file from some VOD services w/o encryption.
+//Download m3u file from some VOD services
 
 
 //For http live streaming service (This script is generally compatible with smartphones and similar devices.)
@@ -6,7 +6,7 @@ const video = document.querySelector('video');
 video && video.src ? console.log("Extracted m3u8 link:", video.src) : console.log("m3u8 link not found.");
 
 
-//For some video services (Generally for PC.)
+//For some video services (From computer for m3u)
 (async () => {
   const m3uUrls = [];
   const entries = performance.getEntries();
@@ -29,5 +29,33 @@ video && video.src ? console.log("Extracted m3u8 link:", video.src) : console.lo
   }
 
   console.log("Found M3U URLs:", m3uUrls);
+
+})();
+
+
+
+//For some video services (From computer for mp4)
+(async () => {
+  const mp4Urls = [];
+  const entries = performance.getEntries();
+
+  for (const entry of entries) {
+    if (entry.name.includes(".mp4")) {
+      mp4Urls.push(entry.name);
+    }
+  }
+
+  for(const url of mp4Urls){
+    try {
+      const response = await fetch(url, { method: 'HEAD' });
+      if (response.ok) {
+        console.log("mp4 URL:", url);
+      }
+    }catch(error) {
+      console.error("error fetching URL:", url, error);
+    }
+  }
+
+  console.log("Found M3U URLs:", mp4Urls);
 
 })();
